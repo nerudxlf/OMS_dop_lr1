a04b4e3c3e4ecddb97bce90c64f0a20c1e0f503b
nerudxlf <nerudxlf@gmail.com>
2020-10-01 15:02:39 +0600

    fix bugs

--- main.py
+++ main.py
@@ -6,13 +6,33 @@ import csv
 import pandas as pd
 
 
+def max_keys(number, dictionary):
+    """
+    this function looks for the maximum key value
+    number: this is the key number
+    dictionary: this is the dictionary:)
+    """
+    for keys_value, age in dictionary.items():    
+        if age == number:
+            return keys_value
+    return 0
+    
+
 def data_to_xls(path, data, sheet_name):
+    """
+    this function is needed to output data to excel
+    path: src
+    data: this is the data:)
+    sheet_name: this is name the table
+    """
     df = pd.DataFrame(data, index=[0]).T
     df.to_excel(path, sheet_name=sheet_name)
 
 
 def draw_func(graph):
-    # this function is drawing png graph
+    """
+    this function is drawing png graph
+    """
     networkx.nx.draw(graph)
     plt.savefig('graph.png')
     plt.close()
@@ -26,9 +46,10 @@ def main():
     user_input = str(input())
     if user_input.lower() == "y":
         draw_func(g)
-    degree = networkx.nx.degree_centrality(g) # степень связанности !!! dict
-    data_to_xls("degree.xls", degree, "Cтепень связанности")
-
+    
+    degreex = dict(networkx.nx.degree(g)) # степень связанности !!! dict
+    data_to_xls("degree.xls", degreex, "Cтепень связанности")
+    
     bet = networkx.nx.betweenness_centrality(g) # степень посредничества !!!dict
     data_to_xls("bet.xls", bet, "степень посредничества")
 
@@ -42,21 +63,24 @@ def main():
     data_to_xls("clustering_coefficient.xls", clustering_coefficient, "коэффициент кластерилизации")
 
     global_clustering_coefficient = np.mean(list(networkx.nx.clustering(g).values())) # глобальный коэффициент кластерилизации !!!number
-    mean_degree = np.mean(list(degree.values())) # средняя степень связанности !!!number
+    mean_degree = np.mean(list(degreex.values())) # средняя степень связанности !!!number
     density_graph = networkx.nx.density(g) # плотность графа !!!number
     diameter_graph = networkx.nx.diameter(g) # диаметр графа !!!number
     number_components = networkx.nx.number_connected_components(g) # число компонентов связанности !!!number
     average_distance = networkx.nx.average_shortest_path_length(g) # среднее расстояние между вершинами !!!number
-
+    max_degree_value = max(list(degreex.values())) # максимальная степень связанности(значение) !!!number
+    max_degree_key = max_keys(max_degree_value, degreex) # ключ максимальной степени связанности !!!number
     dict_data = {
         "глобальный коэффициент кластерилизации": global_clustering_coefficient,
         "средняя степень связанности": mean_degree,
         "плотность графа": density_graph,
         "диаметр графа": diameter_graph,
         "число компонентов связанности": number_components,
-        "среднее расстояние между вершинами": average_distance
+        "среднее расстояние между вершинами": average_distance,
+        "максимальное значнеие": max_degree_value,
+        "ключ": max_degree_key
     }
-    data_to_xls("data.xls", dict_data, "data")
+    data_to_xls(path, dict_data, "data")
 
 
 if __name__ == '__main__':
